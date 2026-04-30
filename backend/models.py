9a8801c6baa5d1ba60bffe8cab00e161fe04a60d
JavierHdzFlores <javierherflores34@gmail.com>
2026-04-30 15:05:55 -0600

    Agregamos mejoras al CU-03  CU-04 de nuevo

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey, Boolean
+from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey, Boolean, UniqueConstraint
 from sqlalchemy.orm import relationship
 from database import Base
 from pydantic import BaseModel
