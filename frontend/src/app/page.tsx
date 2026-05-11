eb08a9bbb55e464ce87dfbe505c983a98baec516
JavierHdzFlores <javierherflores34@gmail.com>
2026-05-11 00:42:07 -0600

    frontend del login

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -8,23 +8,12 @@ import { Footer } from "./components/Footer";
 import { LoginModal}  from "./components/LoginModal";
 
 export default function Home() {
-  // Aquí controlamos si el modal se muestra o no
-  const [isLoginOpen, setIsLoginOpen] = useState(false);
+
   
   return (
     <main className="min-h-screen bg-[#080b14] text-white selection:bg-[#f9a825] selection:text-black">
 
-      {/* --- EJEMPLO DE BOTÓN PARA ABRIRLO --- */}
-      {/* Normalmente este botón iría adentro de tu Navbar */}
-      <div className="p-6 flex justify-end max-w-[1400px] mx-auto">
-        <button 
-          onClick={() => setIsLoginOpen(true)}
-          className="bg-[#E50914] text-white px-6 py-2 rounded-xl font-bold tracking-widest uppercase hover:bg-red-700 transition-colors"
-          style={{ fontFamily: "'Barlow Condensed', sans-serif" }}
-        >
-          Iniciar Sesión
-        </button>
-      </div>
+
 
       {/* 1. Nuestro menú de navegación superior */}
       <Navbar />
@@ -35,9 +24,6 @@ export default function Home() {
       {/* 3. Nuestra sección de Cartelera y Horarios */}
       <MovieGrid />
 
-      <div className="h-screen flex items-center justify-center">
-        <h1 className="text-white/20 text-4xl font-bold">CONTENIDO DE NEXO CINEMA</h1>
-      </div>
 
       {/* 2. Lo colocamos hasta abajo */}
       <Footer />
@@ -45,10 +31,7 @@ export default function Home() {
       {/* Montamos el modal al final. 
         Le pasamos el estado isOpen y la función para cerrarlo.
       */}
-      <LoginModal 
-        isOpen={isLoginOpen} 
-        onClose={() => setIsLoginOpen(false)} 
-      />
+      
     </main>
   );
 }
